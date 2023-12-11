7e5a4a71d25045edd5ee7644d3ef268fcfab6072
“krupaSekhar1” <krupasekhar124@gmail.com>
2023-12-11 05:15:42 +0000

    solution

---
+++ src/main/java/com/example/geohub/GeoHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.geohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoHubApplication.class, args);
+	}
+
+}
